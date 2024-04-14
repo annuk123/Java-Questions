78ed0021928634ff9f3c0c8a1be73e680303576e
Tito Joctan <perfect.altruistic@egmail.com>
2024-04-14 16:38:40 +0300

    1.3: Display a Pattern

---
+++ I/fundamentals/problems/DisplayPattern.java
@@ -0,0 +1,12 @@
+public class DisplayPattern {
+    public static void main(String[] args) {
+        // JAVA pattern
+        // Empty line
+        System.out.println();
+        System.out.println("    J     A   V     V   A");
+        System.out.println("    J    A A   V   V   A A");
+        System.out.println("    J   AAAAA   V V   AAAAA");
+        System.out.println("J J J  A     A   V   A     A");
+                
+    }
+}
