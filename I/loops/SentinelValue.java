2ad1b2907455f2224b6e4c37592bb21b6bb4aeab
Tito Malima Joctan <perfect.altruistic@gmail.com>
2024-04-23 00:42:23 +0300

    listing 5.5: sentinel values

---
+++ I/loops/SentinelValue.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SentinelValue {
+    /** Main method */
+    public static void main(String[] args) {
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Read an initial data
+        System.out.print("Enter an integer (the input ends if it is 0): ");
+        int data = input.nextInt();
+
+        // Keep reading data until the input is 0
+        int sum = 0;
+        while (data != 0) { // Loop
+            sum += data;
+            // Read the next data
+            System.out.print("Enter an integer (the input ends if it is 0): ");
+            data = input.nextInt();
+        } // End of loop
+
+        // Display the result
+        System.out.println("The sum is " + sum);
+    }
+}
